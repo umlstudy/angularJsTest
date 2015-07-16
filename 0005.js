653ad1d335e870cc2d27aa06db50ec1d708e23ae
sejong <umlstudy@gmail.com>
2015-07-16 16:49:00 +0900

    init

---
+++ 0005.js
@@ -0,0 +1,10 @@
+describe('PhoneListCtrl', function(){
+
+	  it('should create "phones" model with 3 phones', function() {
+	    var scope = {},
+	        ctrl = new PhoneListCtrl(scope);
+
+	    expect(scope.phones.length).toBe(3);
+	  });
+
+	});
